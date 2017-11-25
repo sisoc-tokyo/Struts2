904900d1379422ae5dff5682a4fc613ac993b284
Fujimoto Mariko <marikof@marikof.local>
2017-11-25 12:09:49 +0900

    add signature for URL encode

--- all/myfilter/ServletFilterForOGNL.java
+++ all/myfilter/ServletFilterForOGNL.java
@@ -7,10 +7,11 @@ import java.util.regex.Pattern;
 
 public class ServletFilterForOGNL implements javax.servlet.Filter {
 	private static final String SIGNATURE_OGNL = 
-			"OgnlContext|OgnlUtil|#context|@DEFAULT_MEMBER_ACCESS|#_memberAccess|java.lang.ProcessBuilder|java.lang.Runtime";
+			"OgnlContext|OgnlUtil|#context|@DEFAULT_MEMBER_ACCESS|#_memberAccess|java.lang.ProcessBuilder|java.lang.Runtime|%23context|%40DEFAULT_MEMBER_ACCESS|%23_memberAccess|java%2elang%2eProcessBuilder|java%2elang%2eRuntime";
 	private static final String ERROR_INVALID_REQUEST = "BlockedByServletFilterForOGNL.Please press back button.";
 	private static final String filterName="ServletFilterForOGNL";
 	private FilterConfig filterConfig;
+	private Pattern p = Pattern.compile(SIGNATURE_OGNL);
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -24,20 +25,21 @@ public class ServletFilterForOGNL implements javax.servlet.Filter {
 		String query = httpRequest.getQueryString();
 
 		// body
-		StringBuilder body = new StringBuilder("");
+		//StringBuilder body = new StringBuilder("");
+		String body="";
 		httpRequest.setCharacterEncoding("UTF-8");
 		   Enumeration names = httpRequest.getParameterNames();
 		    while (names.hasMoreElements()){
 		      String name = (String)names.nextElement();
 		      String vals[] = request.getParameterValues(name);
 		      if (vals != null){
-		        for (int i = 0 ; i < vals.length ; i++){
-		          body.append(vals[i]);
+		        for (String s : vals){
+		          //body.append(vals[i]);
+		          body+=s;
 		        }
 		      }
 		    }
-		
-		Pattern p = Pattern.compile(SIGNATURE_OGNL);
+		//System.out.println("body:" +body );
 
 		try{
 		// header
@@ -59,8 +61,7 @@ public class ServletFilterForOGNL implements javax.servlet.Filter {
 		} else if (null != query && p.matcher(query).find()) {
 			System.out.println(filterName+":Malicious query:" + query);
 			throw new ServletException(ERROR_INVALID_REQUEST);
-		} 
-		else if (p.matcher(body).find()) {
+		} else if (p.matcher(body).find()) {
 			System.out.println(filterName+":Malicious body:" + body);
 			throw new ServletException(ERROR_INVALID_REQUEST);
 		}
